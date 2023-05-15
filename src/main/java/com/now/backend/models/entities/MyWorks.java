03ff765ff24ef922a3c02fff7343baba46140ebc
Adela Joldic <adela.joldic@stu.ssst.edu.ba>
2023-05-15 23:54:39 +0200

    Unit tests, controller, service, repository, entity added

---
+++ src/main/java/com/now/backend/models/entities/MyWorks.java
@@ -0,0 +1,34 @@
+package com.now.backend.models.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "my-works")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MyWorks {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(name = "opportunity")
+
+    private String opportunity;
+    @Column(name = "organization")
+
+    private String organization;
+    @Column(name = "dateCreated")
+
+    private String dateCreated;
+    @Column(name = "pts")
+
+    private int pts;
+    @Column(name = "status")
+
+    private String status;
+}
--- src/main/java/com/now/backend/services/MyWorksService.java
+++ src/main/java/com/now/backend/services/MyWorksService.java
@@ -1,26 +1,34 @@
 package com.now.backend.services;
 
 import com.now.backend.models.MyWorksDto;
+import com.now.backend.models.entities.MyWorks;
+import com.now.backend.repositories.MyWorksRepository;
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
-@Service
 
+@Service
 public class MyWorksService {
+    private final MyWorksRepository myWorksRepository;
+
+    public MyWorksService(MyWorksRepository myWorksRepository) {
+        this.myWorksRepository = myWorksRepository;
+    }
 
     public List<MyWorksDto> getMyWorks() {
-        List<MyWorksDto> myWorksDtoList = new ArrayList<>();
-        MyWorksDto studentWorks = new MyWorksDto("Desing1", "MOP", "03.01.2023",
-                50, "APPROVED");
-        myWorksDtoList.add(studentWorks);
-        MyWorksDto studentWorks2 = new MyWorksDto("Desing2", "MOP", "04.01.2023",
-                60, "APPROVED");
-        myWorksDtoList.add(studentWorks2);
-        MyWorksDto studentWorks3 = new MyWorksDto("Desing3", "MOP", "05.01.2023",
-                55, "APPROVED");
-        myWorksDtoList.add(studentWorks3);
+        List<MyWorksDto> MyWorksList = new ArrayList<>();
+        List<MyWorks> works = MyWorksRepository.findAll();
+        for (MyWorks work : works) {
+            MyWorksList.add(new MyWorksDto(
+                    work.getOpportunity(),
+                    work.getOrganization(),
+                    work.getDateCreated(),
+                    work.getPts(),
+                    work.getStatus()));
+        }
 
-        return myWorksDtoList;
+        return MyWorksList;
     }
 }
+
